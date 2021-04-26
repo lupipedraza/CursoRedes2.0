1f5834276d0b281013ccca826646d794b36f9f43
LPedraza <lupipedraza@gmail.com>
2021-04-26 18:52:49 -0300

    arreglitos

--- prueba.py
+++ prueba.py
@@ -5,16 +5,30 @@ Created on Fri Apr 23 18:23:39 2021
 
 @author: lucia
 """
+import codigo_General
+
+
+
+#Nombres de los archivos
 archivo_tweets='PruebaLarreta_Tweets.txt'
 archivo_guardado='PruebaLarreta_procesado.txt'
 archivo_usuarios='PruebaLarreta_usuarios.json'
 archivo_grafo='PruebaLarreta_grafo.gexf'
 archivo_grafo_hash='PruebaLarreta_grafo_hash.gexf'
 
-procesamiento(archivo_tweets,archivo_guardado ,archivo_usuarios)
-Larreta=Bases_Datos()
+
+#De la bajada de datos no cambie nada.
+
+#Aca procesa y arma 1 csv con los tweets y un json con los usuarios 
+codigo_General.procesamiento(archivo_tweets,archivo_guardado ,archivo_usuarios)
+
+
+#Acá se arma el objeto y se cargan los datos (se podría hacer todo junto si queremos)
+Larreta=codigo_General.Bases_Datos()
 Larreta.cargar_datos(archivo_guardado)
 Larreta.cargar_usuarios(archivo_usuarios)
+
+#Funciones
 Larreta.plot_tipo_tweet()
 Larreta.plot_evolucion_temporal()
 Larreta.armar_grafo(tipo='usuarios',archivo_grafo=archivo_grafo,tipo_enlace='',dirigido=False)
